df975602afe32e3bb2a662689b82234fea8fc251
hamidreza kalbasi <hamidrezakalbasi@protonmail.com>
2020-11-05 19:10:09 +0330

    initial commit

---
+++ react-ssg.mjs
@@ -0,0 +1,28 @@
+import { cli } from "react-ssg/server.mjs";
+import path from "path";
+
+const dirname = path.dirname(new URL(import.meta.url).pathname);
+process.on('unhandledRejection', (up) => { throw up; });
+
+cli({
+  path: {
+    root: dirname,
+  },
+  webpack: {
+    baseConfig: {
+      plugins: [],
+      module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+          },
+        ]
+      }
+    },
+  },
+  urls: () => [
+    "/", "/about/", "/blog/",
+  ],
+});
